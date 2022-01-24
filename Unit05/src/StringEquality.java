71a187527b74875b45131268ee76556fe96582b6
Declimator <31232768+DCLMTR@users.noreply.github.com>
2022-01-24 09:27:02 -0800

    Unit05 Completed

--- Unit05/src/StringEquality.java
+++ Unit05/src/StringEquality.java
@@ -18,19 +18,25 @@ public class StringEquality
 
 	public StringEquality(String one, String two)
 	{
+		setWords(one, two);
 	}
 
 	public void setWords(String one, String two)
 	{
+		wordOne = one;
+		wordTwo = two;
 	}
 
 	public boolean checkEquality( )
 	{
-		return false;
+		return wordOne.equals(wordTwo);
 	}
 
 	public String toString()
 	{
+		if(checkEquality()) {
+			return wordOne + " has the same letters as " + wordTwo + "\n";
+		}
 		return wordOne + " does not have the same letters as " + wordTwo + "\n";
 	}
 }
--- Unit05/src/StringEqualityRunner.java
+++ Unit05/src/StringEqualityRunner.java
@@ -13,6 +13,13 @@ public class StringEqualityRunner
 	public static void main( String args[] )
 	{
 		//add test cases
-		
+		System.out.println(new StringEquality("hello", "goodbye"));
+		System.out.println(new StringEquality("one", "two"));
+		System.out.println(new StringEquality("three", "four"));
+		System.out.println(new StringEquality("TCEA", "UIL"));
+		System.out.println(new StringEquality("State", "Champions"));
+		System.out.println(new StringEquality("ABC", "ABC"));
+		System.out.println(new StringEquality("ABC", "CBA"));
+		System.out.println(new StringEquality("Same", "Same"));
 	}
 }
