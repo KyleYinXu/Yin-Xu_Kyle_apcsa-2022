9795a2b11125c602b46000a4f7b6f2b50525f14a
Declimator <31232768+DCLMTR@users.noreply.github.com>
2022-01-25 09:28:00 -0800

    Unit 06 Countpairs, DecreasingWords

---
+++ Unit06/src/TriangleOneRunner.java
@@ -0,0 +1,18 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+import static java.lang.System.*;
+
+public class TriangleOneRunner
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		new TriangleOne("hippo");
+		new TriangleOne("abcd");
+		new TriangleOne("it");
+		new TriangleOne("a");
+		new TriangleOne("chicken");
+	}
+}
