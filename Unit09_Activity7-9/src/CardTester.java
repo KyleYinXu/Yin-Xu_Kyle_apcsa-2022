71f953f3c3275ed8f5c5aa3c21ff41587c903383
Declimator <31232768+DCLMTR@users.noreply.github.com>
2022-03-14 09:28:28 -0700

    Unit09 Activities 7-9 in progress

---
+++ Unit09_Activity7-9/src/CardTester.java
@@ -0,0 +1,22 @@
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card cardOne = new Card("Ace", "Spades", 1);
+		Card cardTwo = new Card("Ace", "Spades", 1);
+		Card cardThree = new Card("King", "Hearts", 13);
+		System.out.println(cardOne.rank());
+		System.out.println(cardOne.matches(cardTwo));
+		System.out.println(cardOne);
+		System.out.println(cardTwo);
+		System.out.println(cardThree);
+	}
+}
