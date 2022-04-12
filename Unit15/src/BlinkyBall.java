2de4a63d303e5d595e72bdad886acd550357428f
Declimator <31232768+DCLMTR@users.noreply.github.com>
2022-04-11 23:39:27 -0700

    Unit15 Completed

--- Unit15/src/BlinkyBall.java
+++ Unit15/src/BlinkyBall.java
@@ -17,31 +17,40 @@ class BlinkyBall extends Ball
 
    public BlinkyBall(int x, int y)
    {
-
-
+	   setX(x);
+	   setY(y);
 
    }
 
    public BlinkyBall(int x, int y, int wid, int ht)
    {
-
-
+	   setX(x);
+	   setY(y);
+	   setWidth(wid);
+	   setHeight(ht);
 
    }
 
    public BlinkyBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
    {
-
-
+	   setX(x);
+	   setY(y);
+	   setWidth(wid);
+	   setHeight(ht);
+	   setXSpeed(xSpd);
+	   setYSpeed(ySpd);
 
    }
 
    public BlinkyBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
    {
-
-
-
-
+	   setX(x);
+	   setY(y);
+	   setWidth(wid);
+	   setHeight(ht);
+	   setColor(col);
+	   setXSpeed(xSpd);
+	   setYSpeed(ySpd);
    }
 
    public Color randomColor()
@@ -49,13 +58,19 @@ class BlinkyBall extends Ball
    	int r = 0;		//use Math.random()
  		int g = 0;
  		int b = 0;
+ 		r =  (int) (Math.random()*255);
+ 		g =  (int) (Math.random()*255);
+ 		b =  (int) (Math.random()*255);
  		return new Color(r,g,b);
    }
 
    public void moveAndDraw(Graphics window)
    {
-
-
+	   draw(window, Color.WHITE);
+	      setX(getX()+getXSpeed());
+	      setY(getY()+getYSpeed());
+	      setColor(randomColor());
+	      draw(window, getColor());
 
 
    }
--- Unit15/src/Pong.java
+++ Unit15/src/Pong.java
@@ -29,7 +29,7 @@ public class Pong extends Canvas implements KeyListener, Runnable
 	public Pong()
 	{
 		//set up all variables related to the game
-		ball = new Ball(400, 300, 20, 20, Color.BLUE, 2, 2);
+		ball = new BlinkyBall(400, 300, 20, 20, Color.BLUE, 2, 2);
 		leftPaddle = new Paddle(20, 200, 20, 80, Color.ORANGE, 5);
 		rightPaddle = new Paddle(785-20-20, 200, 20, 80, Color.ORANGE, 5);
 		topWall = new Wall(0, 0, 785, 10);
